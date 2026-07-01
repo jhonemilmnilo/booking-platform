ca9c5226db44f8b83e110821146d3e2cec62dc8c
Rogelio Angelo V. Duque <128900461+RogelioAngelo@users.noreply.github.com>
2026-07-01 09:52:20 +0800

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,11 @@
-import Header from "@/components/shared/Header";
+import { ReactNode } from "react";
 
 export default function MainLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
-    <>
-      <Header />
-      <main className="flex-grow">{children}</main>
-    </>
+    <main>{children}</main>
   );
 }
